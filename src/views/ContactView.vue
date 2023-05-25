ce41f4c1fafd77e09861567cbc615cdf41b35218
AmandaVazquezM <amandavm35@gmail.com>
2023-05-25 21:04:10 +0200

    contact

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="contact-page d-flex flex-column min-vh-100 mt-5">
     <h1>Contacta con nosotros</h1>
-    <form @submit="submitForm">
+    <form @submit.prevent="submitForm">
       <div class="form-group">
         <label for="name">Nombre:</label>
         <input type="text" id="name" v-model="formData.name" required class="form-control">
@@ -22,14 +22,43 @@
   </div>
 </template>
 
+<script>
+export default {
+  data() {
+  return {
+    formData: {
+      name: '', // Initialize with an empty string or provide a default value
+      email: '',
+      message: ''
+    },
+    isSubmitted: false
+  };
+},
+  methods: {
+    submitForm() {
+      
+      const url = 'https://iw4tivf27yl6jlhydikwsafqgm0giqut.lambda-url.eu-west-2.on.aws/'; // funcion Lambda
 
-<style scoped>
-.contact-page {
-  max-width: 500px;
-  margin: 0 auto;
-}
+      const data = JSON.stringify(this.formData);
 
-.form-group {
-  margin-bottom: 20px;
-}
-</style>
+      fetch(url, {
+        method: 'POST',
+        body: data,
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
+      .then(response => {
+        if (response.ok) {
+          this.isSubmitted = true;
+        } else {
+          throw new Error('Error al enviar el formulario');
+        }
+      })
+      .catch(error => {
+        console.error(error);
+      });
+    }
+  }
+};
+</script>
