cf5d53257b5a068f35cb58b102b21906f553b3d6
AmandaVazquezM <amandavm35@gmail.com>
2023-06-01 23:28:41 +0200

    prueba4

--- src/services/GamesService.js
+++ src/services/GamesService.js
@@ -5,37 +5,37 @@ export default {
         {
           name: '¿Sabes el significado?',
           category: 'semantica',
-          image: '../assets/imgs/sinonimos.png',
+          image: require('../assets/imgs/sinonimos.png'),
           component:'synom'
         },
         {
           name: 'Verdadero o falso',
           category: 'gramatica',
-          image:'../assets/imgs/Captura.png',
+          image:require('../assets/imgs/Captura.png'),
           component: 'true'
         },
         {
           name: 'Adivina la palabra',
           category: 'comprension_lectora',
-          image:'../assets/imgs/palabras.png',
+          image:require('../assets/imgs/palabras.png'),
           component: 'phrase'
         },
         {
           name: 'Trivia',
           category: 'ortografia',
-          image:'../assets/imgs/Trivia.png',
+          image:require('../assets/imgs/Trivia.png'),
           component: 'trivia'
         },
         {
           name: 'Elige la opción correcta',
           category: 'comprension_lectora',
-          image:'../assets/imgs/AoB.png',
+          image:require('../assets/imgs/AoB.png'),
           component: 'choose'
         },
         {
           name: 'Elige la letra correcta',
           category: 'ortografia',
-          image:'../assets/imgs/letras.png',
+          image:require('../assets/imgs/letras.png'),
           component: 'character'
         }
       ];
